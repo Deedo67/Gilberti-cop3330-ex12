3f15123dca1f6a128c15139ca099bb382f487a9b
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 22:31:56 -0400

    Initial commit

---
+++ src/main/java/MPE12/example/ex12.java
@@ -0,0 +1,25 @@
+package MPE12.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+
+//class begin
+public class ex12 {
+    //main method begin
+    public static void main(String[] args) {
+        //scanner
+        Scanner in = new Scanner(System.in);
+
+        //input
+        System.out.print("Enter the principal: ");
+        String prin = in.nextLine();
+        System.out.print("Enter the rate of interest: ");
+        String rate = in.nextLine();
+        System.out.print("Enter the number of years: ");
+        String years = in.nextLine();
+    }
+}
